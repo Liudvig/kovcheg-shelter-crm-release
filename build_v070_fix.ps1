fed5745e4ca2b800dc75c862a437927bdb7b6308
Liudvig <71966781+Liudvig@users.noreply.github.com>
2026-08-19 17:05:53 +0300

    Make Epoha 0.7.0 logo patch robust

--- build_v070_fix.ps1
+++ build_v070_fix.ps1
@@ -1,9 +1,11 @@
 $ErrorActionPreference = "Stop"
 
 $script = Get-Content build_v070.ps1 -Raw -Encoding UTF8
-$old = '$logoPattern=''Label logo = new Label\(\); logo\.Text="Э"; logo\.TextAlign=ContentAlignment\.MiddleCenter; logo\.SetBounds\(14,15,50,50\); logo\.Font=F\(24,FontStyle\.Bold\); logo\.ForeColor=Color\.FromArgb\(20,16,28\); logo\.BackColor=Accent; brandHeader\.Controls\.Add\(logo\);'''
-$new = '$logoPattern=''(?s)Label logo = new Label\(\);.*?brandHeader\.Controls\.Add\(logo\);'''
-if (-not $script.Contains($old)) { throw "0.7.0 logo-pattern line not found" }
-$script = $script.Replace($old, $new)
+$start = $script.IndexOf('$logoPattern=')
+if ($start -lt 0) { throw "0.7.0 logo pattern assignment not found" }
+$lineEnd = $script.IndexOf("`n", $start)
+if ($lineEnd -lt 0) { $lineEnd = $script.Length }
+$newLine = '$logoPattern=''(?s)Label logo = new Label\(\);.*?brandHeader\.Controls\.Add\(logo\);'''
+$script = $script.Substring(0,$start) + $newLine + "`r`n" + $script.Substring($lineEnd + 1)
 Set-Content -Path build_v070_runtime.ps1 -Value $script -Encoding UTF8
 .\build_v070_runtime.ps1
