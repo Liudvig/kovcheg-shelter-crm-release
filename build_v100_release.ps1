1f8789c2902092d9a351d75a64063b4cfb09f208
Liudvig <71966781+Liudvig@users.noreply.github.com>
2026-08-19 19:24:00 +0300

    Harden generated C# quote cleanup

--- build_v100_release.ps1
+++ build_v100_release.ps1
@@ -3,6 +3,11 @@ $ErrorActionPreference = "Stop"
 $src = Get-Content ProgramV100Build.cs -Raw -Encoding UTF8
 $names = @('clubModeEnabled','watchdogEnabled','replaceExplorerShell','closeExplorerInClubMode')
 foreach($name in $names){
-    $src = $src.Replace(('\"'+$name+'\"'),('"'+$name+'"'))
+    $clean='"'+$name+'"'
+    $src=$src.Replace(('\"'+$name+'\"'),$clean)
+    $src=$src.Replace(('\\"'+$name+'\\"'),$clean)
+    $src=$src.Replace(('\\\"'+$name+'\\\"'),$clean)
 }
 Set-Content -Path ProgramV100Build.cs -Value $src -Encoding UTF8
+$lines=Get-Content ProgramV100Build.cs
+for($i=168;$i -le 230 -and $i -le $lines.Count;$i++){Write-Host (("{0}: {1}" -f $i,$lines[$i-1]))}
